34cbcab5b9224b19fa9894d19f5002c0395c1f4e
Slava Startsev <slava.startsev@tuta.io>
2022-11-23 23:14:39 +0300

    chore: updates

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'OGS-LP',
-      'OGS LP'
+      'OGX-LP',
+      'OGX LP'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
